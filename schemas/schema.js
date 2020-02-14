96f208922566b099cadab614761e60a260d807f9
Kevin Sparling <kevinsparling@gmail.com>
2020-02-14 11:50:55 +0800

    initial commit, express and sanity

--- schemas/schema.js
+++ schemas/schema.js
@@ -7,10 +7,28 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'default',
+  name: 'mySchema',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    {
+      title: "Player",
+      name: "player",
+      type: "document",
+      fields: [
+        {
+          title: "Name",
+          name: "name",
+          type: "string"
+        },
+        {
+          title: "Birth Date",
+          name: "birthdate",
+          type: "date"
+        }
+      ]
+    }
+    
+
   ])
 })
