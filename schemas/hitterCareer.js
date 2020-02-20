a372f7721231b045cb0995747234958ee6157a94
Kevin Sparling <kevinsparling@gmail.com>
2020-02-20 23:02:44 +0800

    updated hitterCareer

---
+++ schemas/hitterCareer.js
@@ -0,0 +1,112 @@
+export default {
+  name: "hitterCareer",
+  type: "document",
+  title: "Hitter Career",
+  fields: [
+    {
+      title: 'Person',
+      name: 'person',
+      type: 'reference',
+      to: [{type: 'person'}]
+    },
+    // {
+    //   title: 'Hitter',
+    //   name: 'hitter',
+    //   type: 'reference',
+    //   to: [{type: 'hitter'}]
+    // },
+    {
+      title: "Games",
+      name: "games",
+      type: "number"
+    },
+    {
+      title: "At Bats",
+      name: "atBats",
+      type: "number"
+    },
+    {
+      title: "Runs",
+      name: "runs",
+      type: "number"
+    },
+    {
+      title: "Hits",
+      name: "hits",
+      type: "number"
+    },
+    {
+      title: "Doubles",
+      name: "doubles",
+      type: "number"
+    },
+    {
+      title: "Triples",
+      name: "triples",
+      type: "number"
+    },
+    {
+      title: "Home Runs",
+      name: "hr",
+      type: "number"
+    },
+    {
+      title: "RBI",
+      name: "rbi",
+      type: "number"
+    },
+    {
+      title: "Stolen Bases",
+      name: "sb",
+      type: "number"
+    },
+    {
+      title: "Caught Stealing",
+      name: "cs",
+      type: "number"
+    },
+    {
+      title: "Strike Outs",
+      name: "so",
+      type: "number"
+    },
+    {
+      title: "Intentional BB",
+      name: "ibb",
+      type: "number"
+    },
+    {
+      title: "Hit by Pitch",
+      name: "hbp",
+      type: "number"
+    },
+    {
+      title: "Sacrifice Hits",
+      name: "sh",
+      type: "number"
+    },
+    {
+      title: "Sacrifice Flys",
+      name: "sf",
+      type: "number"
+    },
+    {
+      title: "Ground into Double Plays",
+      name: "gidp",
+      type: "number"
+    }
+  ],
+  preview: {
+    select: {
+      name: 'person.name',
+      subtitle: 'year'
+    },
+    prepare(selection) {
+      const {name, year} = selection
+      return {
+        title: name,
+        subtitle: year
+      }
+    }
+  }
+}
--- seeds/hitterSeasonSeed.js
+++ seeds/hitterSeasonSeed.js
@@ -8,60 +8,77 @@ const client = sanityClient({
   useCdn: false
 })
 
-const HITTERS_URL = 'http://127.0.0.1:4000/hitters' //from munenori
+const HITTER_URL = 'http://127.0.0.1:4000/hitter/' //from munenori
 const currentHittersQuery = '*[_type == "hitter"]{..., person->}'
-const currentHittersQueryURL = 'https://9rty98wh.api.sanity.io/v1/data/query/development?query=*[_type%20==%20%22hitter%22]{...,%20person-%3E}'
+//const currentHittersQueryURL = 'https://9rty98wh.api.sanity.io/v1/data/query/development?query=*[_type%20==%20%22hitterSeason%22]{...,%20person-%3E}'
 
-fetch(HITTERS_URL).then(res => res.json())
-.then(allTimeHitters => {
-  //console.log(allTimeHitters)
-  client.fetch(currentHittersQuery).then(currentPeople => {
-    //console.log(currentPeople)
-    let allSeasons = []
-    currentPeople.forEach(hitter => {
-      for(let dude of allTimeHitters) { // dude is from munenori, hitter is from sanity
-        if(dude.playerID === hitter.person.bbrefId) {
-          allSeasons.push({
-            _id: dude.playerID + '-' + dude.stint + '-' + dude.yearID,
-            _type: 'hitterSeason',
-            //hitter: {_type: 'reference', _ref: hitter._id},
-            person: {_type: 'reference', _ref: hitter.person._id},
-            year: parseInt(dude.yearID),
-            games: dude.G,
-            atBats: dude.AB,
-            runs: dude.R,
-            hits: dude.H,
-            doubles: dude.doubles,
-            triples: dude.triples,
-            hr: dude.HR,
-            rbi: dude.RBI,
-            sb: dude.SB,
-            cs: dude.CS,
-            so: dude.SO,
-            ibb: dude.IBB,
-            hbp: dude.HBP,
-            sh: dude.SH,
-            sf: dude.SF,
-            gidp: dude.GIDP
-          })
-        }
-      }
+client.fetch(currentHittersQuery).then(currentHitters => {
+  //console.log(currentHitters)
+  return currentHitters
+})
+.then(currentHitters => {
+  currentHitters.forEach(hitter => {
+    fetch(HITTER_URL + hitter.playerID).then(res => res.json())
+    .then(allHitterSeasons => {
+      let careerStats = []
+      allHitterSeasons.forEach(season => {
+        
+      })
     })
-    //console.log(allSeasons)
-    return allSeasons
-  })
-  .then(allSeasons => {
-    let transaction = client.transaction()
-    allSeasons.forEach(doc => {
-      transaction.createOrReplace(doc)
-    })
-    console.log(transaction)
-    //return transaction
-    return transaction.commit()
   })
 })
-.catch(error => {
-  console.log(error)
-})
 
-
+// fetch(HITTERS_URL).then(res => res.json())
+// .then(allTimeHitters => {
+//   //console.log(allTimeHitters)
+//   client.fetch(currentHittersQuery).then(currentPeople => {
+//     //console.log(currentPeople)
+//     let allSeasons = []
+//     currentPeople.forEach(hitter => {
+//       let careerStats = [{
+//         _id: dude.playerID + '-' + 
+//       }]
+//       for(let dude of allTimeHitters) { // dude is from munenori, hitter is from sanity
+//         if(dude.playerID === hitter.person.bbrefId) {
+//           careerStats.push({
+//             _id: dude.playerID + '-' + dude.stint,
+//             _type: 'hitterSeason',
+//             //hitter: {_type: 'reference', _ref: hitter._id},
+//             person: {_type: 'reference', _ref: hitter.person._id},
+//             year: parseInt(dude.yearID),
+//             games: dude.G,
+//             atBats: dude.AB,
+//             runs: dude.R,
+//             hits: dude.H,
+//             doubles: dude.doubles,
+//             triples: dude.triples,
+//             hr: dude.HR,
+//             rbi: dude.RBI,
+//             sb: dude.SB,
+//             cs: dude.CS,
+//             so: dude.SO,
+//             ibb: dude.IBB,
+//             hbp: dude.HBP,
+//             sh: dude.SH,
+//             sf: dude.SF,
+//             gidp: dude.GIDP
+//           })
+//         }
+//       }
+//     })
+//     //console.log(allSeasons)
+//     return allSeasons
+//   })
+//   .then(allSeasons => {
+//     let transaction = client.transaction()
+//     allSeasons.forEach(doc => {
+//       transaction.createOrReplace(doc)
+//     })
+//     console.log(transaction)
+//     //return transaction
+//     return transaction.commit()
+//   })
+// })
+// .catch(error => {
+//   console.log(error)
+// })
